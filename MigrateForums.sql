f7735635354b571f213677efbad6a0902a51d7b9
Sebastian Leupold <leupold@dnnwerk.de>
2016-09-16 19:09:43 +0200

    continued

--- MigrateForums.sql
+++ MigrateForums.sql
@@ -30,7 +30,7 @@ DECLARE @tplBoardID    int = 1;
 DECLARE @tplCategoryID int = 1;
 DECLARE @tplForumID    int = 1;
 /* -------------------------------------------------- */
-
+-- N'0;13;|1134;||'
 -- Create YAF.Net Boards in table yaf_board
 INSERT INTO dbo.yaf_Board 
        (Name, allowThreaded, MembershipAppName, RolesAppName, oModuleID) 
@@ -48,16 +48,68 @@ USING (SELECT @boardID as BoardID, [Name], [Flags], SortOrder FROM dbo.yaf_Acces
 WHEN NOT MATCHED THEN INSERT (  BoardID, [Name], [Flags],   SortOrder) 
                       VALUES (S.BoardID, S.Name, S.Flags, S.SortOrder);
 
--- Copy over Roles
+-- Create default Roles:
 MERGE INTO dbo.yaf_Group T
-USING (SELECT R.RoleName FROM dbo.Roles R JOIN dbo.ModulePermission P ON R.RoleID = P.RoleID AND P.ModuleID = @oModuleID) S 
-   ON T.BoardID = @BoardID AND T.Name = S.RoleName
+USING (SELECT * FROM dbo.yaf_Group WHERE BoardID = @tplBoardID and Flags != 0) S ON T.BoardID = @BoardID AND T.Name = S.Name
+WHEN NOT MATCHED THEN INSERT ( BoardID,   [Name],   [Flags],   PMLimit,   Style,   SortOrder,   Description,   UsrSigChars,   UsrSigBBCodes,   UsrSigHTMLTags,   UsrAlbums,   UsrAlbumImages,   IsHidden,   IsUserGroup) 
+                      VALUES (@BoardID, S.[Name], S.[Flags]  S.PMLimit, S.Style, S.SortOrder, S.Description, S.UsrSigChars, S.UsrSigBBCodes, S.UsrSigHTMLTags, S.UsrAlbums, S.UsrAlbumImages, S.IsHidden, S.IsUserGroup;
+
+-- Create individual Roles:
+MERGE INTO dbo.yaf_Group T
+USING (SELECT RoleName FROM dbo.Roles 
+        WHERE RoleID     IN (SELECT DISTINCT RoleID FROM dbo.ModulePermission P WHERE P.ModuleID = @oModuleID)
+          AND RoleID NOT IN (SELECT RoleID FROM dbo.Roles R JOIN dbo.Portals P ON R.PortalID = P.PortalID JOIN dbo.Modules M ON P.PortalID = M.PortalID  WHERE M.ModuleID = @oModuleID)
+      ) S  ON T.BoardID = @BoardID AND T.Name = S.RoleName
 WHEN NOT MATCHED THEN INSERT (BoardID, [Name],    [Flags], PMLimit, Style, SortOrder, Description, UsrSigChars, UsrSigBBCodes, UsrSigHTMLTags, UsrAlbums, UsrAlbumImages, IsHidden, IsUserGroup) 
-                      VALUES (@BoardID, S.RoleName,    );
+                      VALUES (@BoardID, S.RoleName,     0,       0,  Null,       100,         N'',           0,           N'',            N'',         0,              0,        0,           0);
 
 -- Populate Ranks:
+MERGE INTO dbo.yaf_Rank T
+USING (SELECT * FROM dbo.yaf_Rank WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.Name = S.Name
+WHEN NOT MATCHED THEN INSERT ( BoardID,   [Name],   MinPosts,   RankImage,   Flags,   PMLimit,   Style,   SortOrder,   Description,   UsrSigChars,   UsrSigBBCodes,   UsrSigHTMLTags,   UsrAlbums,   UsrAlbumImages)
+                      VALUES (@BoardID, S.[Name], S.MinPosts, S.RankImage, S.Flags, S.PMLimit, S.Style, S.SortOrder, S.Description, S.UsrSigChars, S.UsrSigBBCodes, S.UsrSigHTMLTags, S.UsrAlbums, S.UsrAlbumImages);
+
 -- Populate Smileys: 
--- Populate SpamWord: 
+MERGE INTO dbo.yaf_Smiley T
+USING (SELECT * FROM dbo.yaf_Smiley WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.SortOrder = S.SortORder
+WHEN NOT MATCHED THEN INSERT ( BoardID,   [Code],   Icon,   Emoticon,   SortOrder)
+                      VALUES (@BoardID, S.[Code], S.Icon, S.Emoticon, S.SortOrder);
+
+-- Populate SpamWords: 
+MERGE INTO dbo.yaf_Spam_Words T
+USING (SELECT * FROM dbo.yaf_Spam_Words WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.SpamWord = S.SpamWord
+WHEN NOT MATCHED THEN INSERT ( BoardID,   SpamWord)
+                      VALUES (@BoardID, S.SpamWord);
+
+-- Populate Replace_Words: 
+MERGE INTO dbo.yaf_Replace_Words T
+USING (SELECT * FROM dbo.yaf_Replace_Words WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.BadWord = S.BadWord
+WHEN NOT MATCHED THEN INSERT ( BoardID,   BadWord,   GoodWord)
+                      VALUES (@BoardID, S.BadWord, S.GoodWord);
+
+-- Populate Extensions: 
+MERGE INTO dbo.yaf_Extension T
+USING (SELECT * FROM dbo.yaf_Extension WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.Extension = S.Extension
+WHEN NOT MATCHED THEN INSERT ( BoardID,   Extension)
+                      VALUES (@BoardID, S.Extension);
+
+-- Populate BBCodes:
+MERGE INTO dbo.yaf_BBCode T
+USING (SELECT * FROM dbo.yaf_BBCode WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.Name = S.Name
+WHEN NOT MATCHED THEN INSERT ( BoardID,   [Name],   Description,   OnClickJS,   DisplayJS,   EditJS,   DisplayCSS,   SearchRegex,   ReplaceRegex,   Variables,   UseModule,   ModuleClass,   ExecOrder)
+                      VALUES (@BoardID, S.[Name], S.Description, S.OnClickJS,   DisplayJS, S.EditJS, S.DisplayCSS, S.SearchRegex, S.ReplaceRegex, S.Variables, S.UseModule, S.ModuleClass, S.ExecOrder);
+
+-- Populate TopicStatus:
+MERGE INTO dbo.yaf_TopicStatus T
+USING (SELECT * FROM dbo.yaf_TopicStatus WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.TopicStatusName = S.TopicStatusName
+WHEN NOT MATCHED THEN INSERT ( BoardID,   TopicStatusName,   defaultDescription)
+                      VALUES (@BoardID, S.TopicStatusName, S.defaultDescription);
+
+-- Populate Registry:
+MERGE INTO dbo.yaf_Registry T
+USING (SELECT * FROM dbo.yaf_Registry WHERE BoardID = @tplBoardID) S ON T.BoardID = @BoardID AND T.[Name] = S.[Name]
+WHEN NOT MATCHED THEN INSERT ( BoardID,   [Name],   Value)
+                      VALUES (@BoardID, S.[Name], S.Value);
 
 -- Populate Aspnet_Roles: 
 DECLARE @appGuid uniqueIdentifier = (SELECT ApplicationID FROM dbo.aspnet_applications WHERE ApplicationName = N'DotNetNuke' OR ApplicationName = N'DNN');
@@ -66,20 +118,42 @@ USING dbo.yaf_Group S ON T.RoleName = S.Name and S.BoardID = @BoardID
 WHEN NOT MATCHED THEN INSERT (ApplicationID, RoleID, RoleName, LoweredRoleName, Description) 
                       VALUES (@appGuid,  NewID(), S.Name, Lower(S.Name), Null);
 
-
 -- Populate Users: 
+   -- guest user:
+MERGE INTO dbo.yaf_user T
+USING (SELECT * FROM dbo.yaf_User WHERE BoardID = @tplBoardID AND Name = N'Guest') S ON T.BoardID = @BoardID and T.Name = S.Name
+WHEN NOT MATCHED THEN INSERT ( BoardID,   ProviderUserKey,   Name,   DisplayName,   Password,   Email,   Joined,   LastVisit,   IP,   NumPosts,   TimeZone,   Avatar,   Signature,   AvatarImage,   AvatarImageType,   RankID,   Suspended,   SuspendedReason,   SuspendedBy,   LanguageFile,   ThemeFile,   TextEditor,   OverridedefaultThemes,   PMNotification,   AutoWatchTopics,   DailyDigest,   NotificationType,   Flags,   Points,   IsApproved,   IsGuest,   IsCaptchaExcluded,   IsActiveExcluded,   IsDST,   IsDirty,   Culture,   IsFacebookUser,   IsTwitterUser,   UserStyle,   StyleFlags,   IsUserStyle,   IsGroupStyle,   IsRankStyle,   IsGoogleUser) 
+                      VALUES (@BoardID, S.ProviderUserKey, S.Name, S.DisplayName, S.Password, S.Email, S.Joined, S.LastVisit, S.IP, S.NumPosts, S.TimeZone, S.Avatar, S.Signature, S.AvatarImage, S.AvatarImageType, S.RankID, S.Suspended, S.SuspendedReason, S.SuspendedBy, S.LanguageFile, S.ThemeFile, S.TextEditor, S.OverridedefaultThemes, S.PMNotification, S.AutoWatchTopics, S.DailyDigest, S.NotificationType, S.Flags, S.Points, S.IsApproved, S.IsGuest, S.IsCaptchaExcluded, S.IsActiveExcluded, S.IsDST, S.IsDirty, S.Culture, S.IsFacebookUser, S.IsTwitterUser, S.UserStyle, S.StyleFlags, S.IsUserStyle, S.IsGroupStyle, S.IsRankStyle, S.IsGoogleUser);
+
+   -- all other users, who ever created a post:
+MERGE INTO dbo.yaf_user T
+USING (SELECT * FROM dbo.vw_xUsers) S ON T.BoardID = @BoardID and T.Name = S.Name
+WHEN NOT MATCHED THEN INSERT ( BoardID,   ProviderUserKey,   Name,   DisplayName,   Password,   Email,   Joined,   LastVisit,   IP,   NumPosts,   TimeZone,   Avatar,   Signature,   AvatarImage,   AvatarImageType,   RankID,   Suspended,   SuspendedReason,   SuspendedBy,   LanguageFile,   ThemeFile,   TextEditor,   OverridedefaultThemes,   PMNotification,   AutoWatchTopics,   DailyDigest,   NotificationType,   Flags,   Points,   IsApproved,   IsGuest,   IsCaptchaExcluded,   IsActiveExcluded,   IsDST,   IsDirty,   Culture,   IsFacebookUser,   IsTwitterUser,   UserStyle,   StyleFlags,   IsUserStyle,   IsGroupStyle,   IsRankStyle,   IsGoogleUser) 
+                      VALUES (@BoardID, ...);
+
 -- Populate UserProfile: 
--- Populate UserGroup:
+MERGE INTO dbo.yaf_userProfile T
+USING (SELECT * FROM dbo.vw_xProfile) S ON T.BoardID = @BoardID and T.UserID = S.UserID
+WHEN NOT MATCHED THEN INSERT ()
+                      VALUES ();
 
--- Copy module settings and permissions:
+-- Populate UserGroups:
+MERGE INTO dbo.yaf_userGroup T
+USING (SELECT UserId, GroupID FROM ) S ON T.UserID = S.UserID and T.GroupID = S.GroupID
+WHEN NOT MATCHED THEN INSERT (UserID, GroupID) VALUES (S.UserID, S.GroupID);
 
--- Copy AF Forum Groups to YAF.Net Categories
+-- Populate aspnet_usersInRoles:
+MERGE INTO dbo.aspnet_usersInRoles T
+USING (SELECT UserId, RoleID FROM ) S ON T.UserID = S.UserID and T.RoleID = S.RoleID
+WHEN NOT MATCHED THEN INSERT (UserID, RoleID) VALUES (S.UserID, S.RoleID);
+
+-- Copy AF Forum Groups to YAF.Net Categories:
 MERGE INTO dbo.yaf_category T
 USING dbo.activeforums_Groups S ON T.BoardID = @BoardID AND S.ModuleID = @oModuleID AND T.Name = S.GroupName 
 WHEN NOT MATCHED THEN INSERT (BoardID, [Name], CategoryImage, SortOrder, PollgroupID, oGroupID) 
                       VALUES (@oModuleID,  GroupName, N'categoryImageSample.gif', S.SortOrder, Null, S.ForumGroupID);
 
--- Copy AF Forums to YAF.Net Forums
+-- Copy AF Forums to YAF.Net Forums:
 MERGE INTO dbo.yaf_forum T
 USING (SELECT F.*, C.CategoryID 
         FROM  dbo.activeforums_Forums F
@@ -89,11 +163,34 @@ WHEN NOT MATCHED THEN INSERT (  CategoryID, ParentID, [Name],  Description, Sort
                       VALUES (S.CategoryID, Null, S.ForumName, S.ForumDesc, S.SortOrder,   4,        0,        0,         1,           0,     Null,        Null,     Null,   Null,                      0, S.ForumID);
 
 -- Copy Threads:
-
+MERGE INTO dbo.yaf_topic T
+USING (SELECT ) S
+WHEN NOT MATCHED THEN INSERT ()
+                      VALUES ();
 -- Copy Posts/replies:
+MERGE INTO dbo.yaf_Message T
+USING (SELECT ) S
+WHEN NOT MATCHED THEN INSERT ()
+                      VALUES ();
+
+-- Copy Attachments:
+MERGE INTO dbo.yaf_Attachment T
+USING (SELECT ) S
+WHEN NOT MATCHED THEN INSERT ()
+                      VALUES ();
 
 -- Copy Notifications (yaf_Watch):
+MERGE INTO dbo.yaf_WatchForum T
+USING (SELECT ) S
+WHEN NOT MATCHED THEN INSERT ()
+                      VALUES ();
 
+MERGE INTO dbo.yaf_WatchTopic T
+USING (SELECT ) S
+WHEN NOT MATCHED THEN INSERT ()
+                      VALUES ();
+
+-- Copy group & forum permission // skipped due to incompatible Permission format, please set manually
 
 Exec dbo.[yaf_forum_resync] @BoardID;
 
