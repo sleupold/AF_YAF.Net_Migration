e06e57639ad5ea2d5c405115608d44c77e8add95
Sebastian Leupold <leupold@dnnwerk.de>
2016-09-16 15:07:21 +0200

    continued

--- MigrateForums.sql
+++ MigrateForums.sql
@@ -24,6 +24,12 @@ GO
 DECLARE @oModuleID int = -1; 
 /*                       ^ replace with your ModuleID */
 /* -------------------------------------------------- */
+/* modify the following id's to match the board, 
+  you want to use as template:                        */
+DECLARE @tplBoardID    int = 1;
+DECLARE @tplCategoryID int = 1;
+DECLARE @tplForumID    int = 1;
+/* -------------------------------------------------- */
 
 -- Create YAF.Net Boards in table yaf_board
 INSERT INTO dbo.yaf_Board 
@@ -37,20 +43,16 @@ DECLARE @boardID int = (SELECT BoardID FROM dbo.yaf_Board WHERE oModuleID = @oMo
 
 -- create accessMasks:
 MERGE INTO dbo.yaf_AccessMask  T
-USING (SELECT @boardID as BoardID, N'Admin Access' AS [Name], 2047 AS Flags, 4 AS SortOrder
-       UNION ALL SELECT @boardID, N'Moderator Access', 1511, 3
-       UNION ALL SELECT @boardID, N'Member Access', 1447, 2,
-       UNION ALL SELECT @boardID, N'Read Only Access', 1, 1,
-       UNION ALL SELECT @boardID, N'No Access', 0, 0, 
-      ) S ON T.BoardID = S.BoardID and T.SortOrder = S.SortOrder
+USING (SELECT @boardID as BoardID, [Name], [Flags], SortOrder FROM dbo.yaf_AccessMask WHERE BoardID = @tplBoardID) S 
+       ON T.BoardID = S.BoardID and T.SortOrder = S.SortOrder
 WHEN NOT MATCHED THEN INSERT (  BoardID, [Name], [Flags],   SortOrder) 
                       VALUES (S.BoardID, S.Name, S.Flags, S.SortOrder);
 
 -- Copy over Roles
-MERGE INTO dbo.yaf_Roles T
-USING (SELECT FROM dbo.Roles R JOIN dbo.ModulePermission P ON R.RoleID = P.RoleID AND P.ModuleID = @oModuleID) S 
+MERGE INTO dbo.yaf_Group T
+USING (SELECT R.RoleName FROM dbo.Roles R JOIN dbo.ModulePermission P ON R.RoleID = P.RoleID AND P.ModuleID = @oModuleID) S 
    ON T.BoardID = @BoardID AND T.Name = S.RoleName
-WHEN NOT MATCHED THEN INSERT (BoardID, [Name],    [Flags], PMLimit, Style, SortOrder, Description, UsrSigChars, UsrSigBBCodes, UsrSigHTMLTags, UsrAlbum, UsrAlbumImages, IsHidden, IsUsrGroup) 
+WHEN NOT MATCHED THEN INSERT (BoardID, [Name],    [Flags], PMLimit, Style, SortOrder, Description, UsrSigChars, UsrSigBBCodes, UsrSigHTMLTags, UsrAlbums, UsrAlbumImages, IsHidden, IsUserGroup) 
                       VALUES (@BoardID, S.RoleName,    );
 
 -- Populate Ranks:
@@ -58,11 +60,11 @@ WHEN NOT MATCHED THEN INSERT (BoardID, [Name],    [Flags], PMLimit, Style, SortO
 -- Populate SpamWord: 
 
 -- Populate Aspnet_Roles: 
-DECLARE @appGuid uniqueIdentifier = (SELECT ApplicationID FROM dbo.aspnet_application WHERE ApplicationName = N'DotNetNuke' OR ApplicationName = N'DNN');
+DECLARE @appGuid uniqueIdentifier = (SELECT ApplicationID FROM dbo.aspnet_applications WHERE ApplicationName = N'DotNetNuke' OR ApplicationName = N'DNN');
 MERGE INTO dbo.aspnet_Roles T
-USING dbo.yaf_Roles S ON T.RoleName = S.RoleName and S.BoardID = @BoardID
+USING dbo.yaf_Group S ON T.RoleName = S.Name and S.BoardID = @BoardID
 WHEN NOT MATCHED THEN INSERT (ApplicationID, RoleID, RoleName, LoweredRoleName, Description) 
-                      VALUES (@appGuid,  NewID(), S.RoleName, Lower(S.RoleName), Null);
+                      VALUES (@appGuid,  NewID(), S.Name, Lower(S.Name), Null);
 
 
 -- Populate Users: 
@@ -81,20 +83,21 @@ WHEN NOT MATCHED THEN INSERT (BoardID, [Name], CategoryImage, SortOrder, Pollgro
 MERGE INTO dbo.yaf_forum T
 USING (SELECT F.*, C.CategoryID 
         FROM  dbo.activeforums_Forums F
-        JOIN  dbo.yaf_categories      C ON F.ForumGroupID = C.oGroupID) S
+        JOIN  dbo.yaf_category        C ON F.ForumGroupID = C.oGroupID) S
    ON S.CategoryID = T.CategoryID AND T.Name = S.ForumName 
-WHEN NOT MATCHED THEN INSERT (  CategoryID, ParentID, [Name],  Description, SortOrder, Flags, IsLocked, isHidden, IsNoCount, IsModerated, ThemeURL, PollGroupID, ImageURL, Styles, IsModeratedNewTopic, oForumID) 
-                      VALUES (S.CategoryID, Null, S.ForumName, S.ForumDesc, S.SortOrder,   4,        0,        0,         1,           0,     Null,        Null,     Null,   Null,                  0, S.ForumID);
+WHEN NOT MATCHED THEN INSERT (  CategoryID, ParentID, [Name],  Description, SortOrder, Flags, IsLocked, isHidden, IsNoCount, IsModerated, ThemeURL, PollGroupID, ImageURL, Styles, IsModeratedNewTopicOnly, oForumID) 
+                      VALUES (S.CategoryID, Null, S.ForumName, S.ForumDesc, S.SortOrder,   4,        0,        0,         1,           0,     Null,        Null,     Null,   Null,                      0, S.ForumID);
 
--- Copy Threads
+-- Copy Threads:
 
--- Copy Posts/replies
+-- Copy Posts/replies:
 
--- Copy Notifications (yaf_Watch)
+-- Copy Notifications (yaf_Watch):
 
-GO
 
 Exec dbo.[yaf_forum_resync] @BoardID;
+
+GO
 
 -- Undo Table modifications:
 IF Exists (SELECT * FROM sys.columns where object_id = OBJECT_ID(N'dbo.yaf_forum') and name = N'oForumID')
